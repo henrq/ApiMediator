335fb2032d366845a55ec673b51b0a9eae44c702
henrq <henrqdefaria@gmail.com>
2023-09-14 08:53:01 -0300

    cria Domain/Customer/Handler EventsHandlers e Notifications

---
+++ Domain/Customer/Handler/CustomerHandler.cs
@@ -0,0 +1,77 @@
+﻿using ApiMediator.Domain.Customer.Command;
+using ApiMediator.Domain.Customer.Entity;
+using ApiMediator.Infra;
+using ApiMediator.Notifications;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApiMediator.Domain.Customer.Handler
+{
+    public class CustomerHandler :
+        IRequestHandler<CustomerCreateCommand, string>,
+        IRequestHandler<CustomerUpdateCommand, string>,
+        IRequestHandler<CustomerDeleteCommand, string>
+    {
+
+        private readonly IMediator _mediator;
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerHandler(IMediator mediator, ICustomerRepository customerRepository)
+        {
+            _mediator = mediator;
+            _customerRepository = customerRepository;
+        }
+
+        public async Task<string> Handle(CustomerCreateCommand request, CancellationToken cancellationToken)
+        {
+            var customer = new CustomerEntity(request.Id, request.FirstName, request.LastName, request.Email, request.Phone);
+            await _customerRepository.Save(customer);
+
+            await _mediator.Publish(new CustomerActionNotification
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Action = ActionNotification.Criado
+            }, cancellationToken);
+
+            return await Task.FromResult("Cliente registrado com sucesso");
+        }
+
+        public async Task<string> Handle(CustomerUpdateCommand request, CancellationToken cancellationToken)
+        {
+            var customer = new CustomerEntity(request.Id, request.FirstName, request.LastName, request.Email, request.Phone);
+            await _customerRepository.Update(request.Id, customer);
+
+            await _mediator.Publish(new CustomerActionNotification
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Action = ActionNotification.Atualizado
+            }, cancellationToken);
+
+            return await Task.FromResult("Cliente atualizado com sucesso");
+        }
+
+        public async Task<string> Handle(CustomerDeleteCommand request, CancellationToken cancellationToken)
+        {
+            var client = await _customerRepository.GetById(request.Id);
+            await _customerRepository.Delete(request.Id);
+
+            //Publish(), responsavél por emitir a notificação em todo sistema,
+            //onde ele vai procurar a classe que possui a herança da interface INotificationHandler<tipo do objeto>
+            //e invocar o método Handler() para processar aquela notificação.
+            await _mediator.Publish(new CustomerActionNotification
+            {
+                FirstName = client.FirstName,
+                LastName = client.LastName,
+                Email = client.Email,
+                Action = ActionNotification.Excluido
+            }, cancellationToken);
+
+            return await Task.FromResult("Cliente excluido com sucesso");
+        }
+    }
+}
---
+++ EventsHandlers/EmailHandler.cs
@@ -0,0 +1,22 @@
+﻿using ApiMediator.Notifications;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApiMediator.EventsHandlers
+{
+    //Qualquer notificação ref. ao customer em nosso sistema, a classe EmailHandler receberá a notificação
+    // e fará a execução do processo
+    public class EmailHandler : INotificationHandler<CustomerActionNotification>
+    {
+        public Task Handle(CustomerActionNotification notification, CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+            {
+                //ao invés de disparar e-mail, fazemos uma simulação com o console
+                Console.WriteLine("O cliente {0} {1} foi {2} com sucesso", notification.FirstName, notification.LastName, notification.Action.ToString().ToLower());
+            });
+        }
+    }
+}
